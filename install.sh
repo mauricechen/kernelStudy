2996d2994c8b51e03044a90bc56205c9e41b362b
maurice chen <maurice.chen.tw@gmail.com>
2020-03-14 15:08:52 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,36 @@
+sudo apt update
+sudo apt install git
+sudo apt install vim       
+sudo apt install vim-gtk3  
+sudo apt install vim-tiny  
+sudo apt install neovim    
+sudo apt install vim-athena
+sudo apt install vim-gtk   
+sudo apt install vim-nox 
+sudo apt install python3
+sudo apt install build-essential
+sudo apt install kernel-package 
+sudo apt install libncurses5-dev 
+sudo apt install libelf-dev
+sudo apt install gcc
+sudo apt install g
+sudo apt install xz-utils 
+sudo apt install ncures-dev
+sudo apt install lss-dev
+sudo apt install flex
+sudo apt install bisio
+sudo apt install bison
+sudo apt install xsltproc
+sudo apt install gperf
+sudo apt install g++
+sudo apt install libc6-dev
+sudo apt install gcc-multlib
+sudo apt install gcc-multilib
+sudo apt install libxml2-utils
+sudo apt install lzma
+sudo apt install m4
+#sudo apt autoremove
+sudo apt install linux-headers-$(uname -r)-generic
+sudo apt install kernel-source-$(uname -r)
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.25.tar.xz
+
